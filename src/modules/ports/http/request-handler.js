8130f654c456041811a70ea1ac232ca8c99cdb6c
Tim Roberts <timiroberts@gmail.com>
2021-02-20 12:34:44 -0500

    Document request handlers

--- src/modules/ports/http/request-handler.js
+++ src/modules/ports/http/request-handler.js
@@ -34,6 +34,44 @@ const set_context = (transformer, after) => [
 
 export default () =>
   R.cond([
+    /**
+     * You can render Pug templates
+     */
+    set_context(is_home, (system) =>
+      system.trigger(respond_to_request(), pug('home', { title: 'Home Page' }))
+    ),
+    /**
+     * You can render JSON
+     */
+    set_context(is_api, (system) =>
+      system.trigger(
+        respond_to_request(),
+        json({
+          status: 200,
+          data: {
+            message: 'You really made it!',
+          },
+        })
+      )
+    ),
+    /**
+     * You can offload the handling of the request
+     * to a different event or even multiple. This
+     * will can be as many events deep as you want as
+     * long as one of them calls respond to request
+     * within TIMEOUT
+     */
+    set_context(is_multiple_events, (system) =>
+      system.trigger(EVENTS.SERVER_KICKOFF_DEMO)
+    ),
+    /**
+     * You can even get fancy and have one request await
+     * another specific request, if that is important to you
+     * for some reason. This sort of awaiting another request
+     * to come in before resolving should only be used in the
+     * most extreme instances and will still be under the TIMEOUT
+     * rule.
+     */
     set_context(is_awaiting_another_event, (system) => {
       // Use closures to ensure you are handling the correct
       // state if you are awaiting another request!
@@ -65,21 +103,4 @@ export default () =>
 
       system.trigger(EVENTS.SERVER_SECOND_REQUEST)
     }),
-    set_context(is_multiple_events, (system) =>
-      system.trigger(EVENTS.SERVER_KICKOFF_DEMO)
-    ),
-    set_context(is_api, (system) =>
-      system.trigger(
-        respond_to_request(),
-        json({
-          status: 200,
-          data: {
-            message: 'You really made it!',
-          },
-        })
-      )
-    ),
-    set_context(is_home, (system) =>
-      system.trigger(respond_to_request(), pug('home', { title: 'Home Page' }))
-    ),
   ])(Context.get(PATH_OF_REQUEST_PATH))
